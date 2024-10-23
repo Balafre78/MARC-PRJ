b989eff73b855d2d84e4a36a6b6b914daad0da22
Emprario <pierre.cameleri@free.fr>
2024-10-23 21:36:38 +0200

    Add essentials headers for the futur runtime

---
+++ utils.c
@@ -0,0 +1,5 @@
+//
+// Created by pierr on 23/10/2024.
+//
+
+#include "utils.h"
---
+++ utils.h
@@ -0,0 +1,17 @@
+//
+// Created by pierr on 23/10/2024.
+//
+
+#ifndef MARC_PRJ_UTILS_H
+#define MARC_PRJ_UTILS_H
+
+#include "moves.h"
+
+/**
+ * @brief Select movements for this turn
+ * @param size amount of mvts to select
+ * @return an array of move
+ */
+t_move *selMoves(int size);
+
+#endif //MARC_PRJ_UTILS_H
