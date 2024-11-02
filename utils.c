f0c1ea76723e9ea2fc034c611e3912b33997fffc
Emprario <pierre.cameleri@free.fr>
2024-11-03 00:12:12 +0100

    add selMoves func

--- utils.c
+++ utils.c
@@ -38,3 +38,29 @@ void printTree(t_tree *tree) {
     printTreeRec(tree->root, tree->root->nbSons, architecture);
     free(architecture);
 }
+
+t_move *selMoves(int size) {
+    int movePool[AMOUNT_MVT] = ARR_MOVEPOOL;
+    srand(SEED);
+
+    int choice, acc, sum;
+    sum = BASE_SUM;
+
+    t_move *moveArr = malloc(size * sizeof(t_move));
+
+    for (int i = 0; i < size; i++) {
+        choice = rand() % sum;
+
+        acc = 0;
+        for (int j = 0; j < 7; j++) {
+            if (acc <= choice && choice < acc + movePool[j]) {
+                movePool[j]--;
+                moveArr[i] = (t_move) j;
+            }
+            acc += movePool[j];
+        }
+        sum--;
+    }
+
+    return moveArr;
+}
--- utils.h
+++ utils.h
@@ -5,8 +5,16 @@
 #ifndef MARC_PRJ_UTILS_H
 #define MARC_PRJ_UTILS_H
 
+#include <stdlib.h>
+
 #include "moves.h"
 #include "tree.h"
+
+#define SEED 255
+#define AMOUNT_MVT 7
+
+#define ARR_MOVEPOOL { 22, 15, 7, 7, 21, 21, 7}
+#define BASE_SUM 100
 
 /**
  * @brief Select movements for this turn
@@ -14,6 +22,13 @@
  * @return an array of move
  */
 t_move *selMoves(int size);
+
+/**
+ * @brief Select movements for this turn
+ * @param size amount of mvts to select
+ * @return an array of move
+ */
+t_move *delMoves(int size);
 
 /**
  * @brief Print a tree in console (stdout)
